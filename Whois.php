f6849df31b54ccc25df8b0bf1850572861e64bd1
ConfuzzledDuck <jonathon@fubra.com>
2017-08-18 14:06:20 +0000

    Added static lookup method, and re-worked server lookup to use it.

--- Whois.php
+++ Whois.php
@@ -12,7 +12,8 @@ class Whois {
 	 // Once deturmined, this is the domain whois server...
 	protected $_whoisServer;
 
-	 // Constructor. Optionally builds with a domain...
+	 // Constructor. Optionally builds with a domain. If instantiated the class acts as a cache
+	 // for whois data. Further requests for data will not go back to the source unless forced.
 	public function __construct($domain = null, $quick = false) {
 
 		if (null !== $domain) {
@@ -32,21 +33,41 @@ class Whois {
 	 // Returns the whois server to query for this TLD...
 	public static function findServer($domain) {
 	
-		exec('whois -h '.self::ROOT_WHOIS_SERVER.' '.$domain, $output, $return);
-		if ((0 == $return) && is_array($output)) {
-			$whoisData = array();
-			foreach ($output AS $outputLine) {
-				if (!empty($outputLine)) {
-					$outputLineElements = preg_split('/: +/', trim($outputLine), 2);
-					if ('whois' == $outputLineElements[0]) {
-						return trim($outputLineElements[1]);
-					}
+		$whoisResult = self::whoisLookup($domain, self::ROOT_WHOIS_SERVER, true);
+		if (false !== $whoisResult && is_array($whoisResult)) {
+			foreach ($whoisResult AS $whoisLine) {
+				$outputLineElements = preg_split('/: +/', trim($whoisLine), 2);
+				if ('whois' == $outputLineElements[0]) {
+					return $outputLineElements[1];
 				}
 			}
 		}
 		
 		return false;
 		
+	}
+	
+	 // Carries out a whois lookup for the given domain on the optional given whois server.
+	 // Returns either the raw output (trimmed lines as an array) or a parsed version.
+	public static function whoisLookup($domain, $whoisServer = null, $raw = false) {
+	
+		if (null == $whoisServer) {
+			$whoisServer = self::findServer($domain);
+		}
+		
+		exec('whois -h '.$whoisServer.' '.$domain, $output, $return);
+		if ((0 == $return) && is_array($output)) {
+			$whoisData = array();
+			foreach ($output AS $outputLine) {
+				if (!empty($outputLine)) {
+					$whoisData[] = trim($outputLine);
+				}
+			}
+			return $whoisData;
+		}
+		
+		return false;
+	
 	}
 	
 }
