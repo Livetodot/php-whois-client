519fed81497687ce47240d0a0062a1090e81d898
ConfuzzledDuck <jonathon@fubra.com>
2017-11-02 00:43:46 +0000

    Added Verisign patterns for registration and expiry dates.

--- patterns/whois.verisign-grs.com.php
+++ patterns/whois.verisign-grs.com.php
@@ -2,4 +2,6 @@
 
 // Parsing patterns for whois.verisign-grs.com.
 
-$parsePatterns = array('registarWhois' => '/Registrar WHOIS Server: (.*)/');
+$parsePatterns = array('registarWhois' => '/Registrar WHOIS Server: (.*)/',
+                       'registrationDate' => '/Creation Date: (.*)/',
+											 'expiryDate' => '/Registry Expiry Date: (.*)/');
